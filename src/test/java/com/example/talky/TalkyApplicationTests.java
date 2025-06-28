8519dd4747c55a4dfae3db29d2d008d39dc79d1e
zzuhannn <lunaaeric41@naver.com>
2025-06-28 14:46:51 +0900

    🎉 프로젝트 초기 설정

---
+++ src/test/java/com/example/talky/TalkyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.talky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
