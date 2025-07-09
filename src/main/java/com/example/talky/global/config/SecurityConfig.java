25373e32a0aa2052af083882bf237778efa38fc8
zzuhannn <lunaaeric41@naver.com>
2025-07-09 18:44:40 +0900

    Refactor : security 주석 추가

--- src/main/java/com/example/talky/global/config/SecurityConfig.java
+++ src/main/java/com/example/talky/global/config/SecurityConfig.java
@@ -20,6 +20,7 @@ public class SecurityConfig {
     }
 
     @Bean
+    // JWT 토큰 형식 수정 예정
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
