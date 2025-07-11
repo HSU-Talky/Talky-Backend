1f6dc069a0b3b84206e1dfcb35cd326189732b53
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-07-11 21:57:02 +0900

    Feat: 서비스 인터페이스에 구현 함수 추가 및 서비스 구현체 내부에서 비즈니스 로직 구현

--- src/main/java/com/example/talky/domain/favorites/service/FavoriteService.java
+++ src/main/java/com/example/talky/domain/favorites/service/FavoriteService.java
@@ -1,8 +1,10 @@
 package com.example.talky.domain.favorites.service;
 
+import com.example.talky.domain.favorites.web.dto.AllFavoriteRes;
 import com.example.talky.domain.favorites.web.dto.CreateFavoriteReq;
 import com.example.talky.domain.favorites.web.dto.CreateFavoriteRes;
 
 public interface FavoriteService {
-    CreateFavoriteRes create(Long nomalId, CreateFavoriteReq createFavoriteReq);
+    CreateFavoriteRes create(Long normalId, CreateFavoriteReq createFavoriteReq);
+    AllFavoriteRes getAllFavorite(Long normalId);
 }
--- src/main/java/com/example/talky/domain/favorites/service/FavoriteServiceImpl.java
+++ src/main/java/com/example/talky/domain/favorites/service/FavoriteServiceImpl.java
@@ -4,11 +4,14 @@ import com.example.talky.domain.auth.entity.NormalUser;
 import com.example.talky.domain.auth.repository.NormalUserRepository;
 import com.example.talky.domain.favorites.entity.Favorite;
 import com.example.talky.domain.favorites.repository.FavoriteRepository;
+import com.example.talky.domain.favorites.web.dto.AllFavoriteRes;
 import com.example.talky.domain.favorites.web.dto.CreateFavoriteReq;
 import com.example.talky.domain.favorites.web.dto.CreateFavoriteRes;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -18,8 +21,9 @@ public class FavoriteServiceImpl implements FavoriteService {
     private final FavoriteRepository favoriteRepository;
     private final NormalUserRepository normalUserRepository;
 
+    // 새 즐겨찾기 생성
     @Override
-    public CreateFavoriteRes create(Long nomalId, CreateFavoriteReq createFavoriteReq) {
+    public CreateFavoriteRes create(Long normalId, CreateFavoriteReq createFavoriteReq) {
 
         /**
          * NormalUserRepository에 UserNotFoundException 없음
@@ -27,7 +31,7 @@ public class FavoriteServiceImpl implements FavoriteService {
          */
 
         NormalUser user = normalUserRepository
-                .findById(nomalId)
+                .findById(normalId)
                 .orElseThrow(RuntimeException::new);
 
         Favorite favorite = Favorite.builder()
@@ -39,5 +43,22 @@ public class FavoriteServiceImpl implements FavoriteService {
         Favorite res = favoriteRepository.save(favorite);
 
         return new CreateFavoriteRes(res.getFavoriteId());
+    }
+
+    // 해당 유저의 모든 즐겨찾기 조회
+    @Override
+    public AllFavoriteRes getAllFavorite(Long normalId) {
+        NormalUser user = normalUserRepository
+                .findById(normalId)
+                .orElseThrow(RuntimeException::new);
+        return new AllFavoriteRes(
+                favoriteRepository.findAllByNormalUserId(normalId).stream()
+                        .map(f -> new AllFavoriteRes.AllFavorite(
+                                f.getSentence(),
+                                f.getCount(),
+                                f.getCreatedAt()
+                        ))
+                        .collect(Collectors.toList())
+        );
     }
 }
