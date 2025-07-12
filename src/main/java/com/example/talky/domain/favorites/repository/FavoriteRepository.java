16601c62c77d897b77448faef967c1374ca86826
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-07-12 11:37:18 +0900

    Feat: 새로운 예외에 필요한 쿼리 메서드 작성

--- src/main/java/com/example/talky/domain/favorites/repository/FavoriteRepository.java
+++ src/main/java/com/example/talky/domain/favorites/repository/FavoriteRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
     List<Favorite> findAllByNormalUserId(Long normalUserId);
+    boolean existsByNormalUserIdAndSentence(Long normalUserId, String sentence);
 }
