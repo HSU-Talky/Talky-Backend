a19a3f20c80478d5cfd4d805c7618de7b03379b6
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-07-13 17:14:05 +0900

    Feat: 컨트롤러 내부 메서드 코드 작성

---
+++ src/main/java/com/example/talky/domain/practice/web/controller/PracticeController.java
@@ -0,0 +1,32 @@
+package com.example.talky.domain.practice.web.controller;
+
+import com.example.talky.domain.practice.service.PracticeService;
+import com.example.talky.domain.practice.web.dto.GetAllRes;
+import com.example.talky.global.response.SuccessResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/prac")
+@RequiredArgsConstructor
+public class PracticeController {
+
+    // 서비스 의존성 명시
+    private final PracticeService practiceService;
+
+    @GetMapping
+    public ResponseEntity<SuccessResponse<?>> getPractice(Long pracId) {
+        // pracId를 서비스 계층에 던짐
+        GetAllRes res = practiceService.getPractice(pracId);
+        // 성공 결과 반환
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(SuccessResponse.ok(res));
+    }
+}
