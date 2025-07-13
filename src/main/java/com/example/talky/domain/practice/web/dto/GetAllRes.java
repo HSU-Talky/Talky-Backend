3473abade85b022592f005750098bfb3fc5012d0
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-07-13 17:35:41 +0900

    Fix: question List -> 단일 객체로 수정

--- src/main/java/com/example/talky/domain/practice/web/dto/GetAllRes.java
+++ src/main/java/com/example/talky/domain/practice/web/dto/GetAllRes.java
@@ -5,16 +5,12 @@ import com.example.talky.domain.practice.domain.Question;
 import java.util.List;
 
 public record GetAllRes(
-        List<Question> all
+        Question question,
+        List<AnswerSet> answers
 ) {
-    public record AllQuestion(
-            Long id,
-            String question,
-            List<AnswerSet> answers
-    ) {
         public record AnswerSet(
                 String answer,
                 Integer nextQuestionId
         ) {}
-    }
 }
+
