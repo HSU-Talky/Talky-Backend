319a97f919da86ab80112705065ef9f448ad7264
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-07-13 17:13:36 +0900

    Feat: 서비스 계층 내부의 비즈니스 로직 구현

---
+++ src/main/java/com/example/talky/domain/practice/service/PracticeService.java
@@ -0,0 +1,7 @@
+package com.example.talky.domain.practice.service;
+
+import com.example.talky.domain.practice.web.dto.GetAllRes;
+
+public interface PracticeService {
+    GetAllRes getPractice(Long pracId);
+}
---
+++ src/main/java/com/example/talky/domain/practice/service/PracticeServiceImpl.java
@@ -0,0 +1,36 @@
+package com.example.talky.domain.practice.service;
+
+import com.example.talky.domain.practice.repository.AnswerRepository;
+import com.example.talky.domain.practice.repository.QuestionRepository;
+import com.example.talky.domain.practice.web.dto.GetAllRes;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PracticeServiceImpl implements PracticeService {
+
+    private final QuestionRepository questionRepository;
+    private final AnswerRepository answerRepository;
+
+    // pracId를 이용한 상황별 질문 및 답변 조회 로직. nextQuestionId를 던져서, 프론트엔드로부터 다음 질문을 연결하게끔 할 예정
+    @Override
+    public GetAllRes getPractice(Long pracId) {
+        return new GetAllRes(
+                questionRepository.findById(pracId).stream()
+                        .map(q -> new GetAllRes.AllQuestion(
+                                q.getId(),
+                                q.getSentence(),
+                                q.getAnswers().stream()
+                                        .map(a -> new GetAllRes.AllQuestion.AnswerSet(
+                                                a.getAnswer(),
+                                                a.getNextQuestionId()
+                                        ))
+                                        .collect(Collectors.toList())))
+                        .collect(Collectors.toList()));
+    }
+}
