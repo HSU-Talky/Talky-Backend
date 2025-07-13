596ce3a3c5762e39d9d19a0b24efb2abc8a53da1
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-07-13 17:34:11 +0900

    Fix: Question 클래스 내부 answers 필드 수정

--- src/main/java/com/example/talky/domain/practice/domain/Question.java
+++ src/main/java/com/example/talky/domain/practice/domain/Question.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -21,5 +22,5 @@ public class Question {
     private String sentence;
 
     @OneToMany(mappedBy = "question")
-    private List<Answer> answers;
+    private List<Answer> answers = new ArrayList<>();
 }
