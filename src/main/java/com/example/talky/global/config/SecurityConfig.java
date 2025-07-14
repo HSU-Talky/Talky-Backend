7ddfde9261ebabaaed13c27b8c0f588ca78fc3e3
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-07-14 10:37:49 +0900

    Feat: prac 접근 권한을 all로 부여

--- src/main/java/com/example/talky/global/config/SecurityConfig.java
+++ src/main/java/com/example/talky/global/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/favorite/**").permitAll()
+                        .requestMatchers(("/prac/**")).permitAll()
                         .anyRequest().authenticated());
 
         return http.build();
