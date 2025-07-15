e303590ab5f7432354230ab21db4d41a9acb5488
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-07-16 01:40:03 +0900

    Fix: 잘못 선언되어있던 enum 수정

--- src/main/java/com/example/talky/domain/practice/exception/PracticeErrorCode.java
+++ src/main/java/com/example/talky/domain/practice/exception/PracticeErrorCode.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum PracticeErrorCode implements BaseResponseCode {
-    PRACTICE_INVALID_HTTP_PARAM("PRACTICE_INVALID_HTTP_PARAM_400", 400, "요청하신 파라미터 값이 잘못되었습니다."),
+    PRACTICE_INVALID_HTTP_PARAM_400("PRACTICE_INVALID_HTTP_PARAM_400", 400, "요청하신 파라미터 값이 잘못되었습니다."),
     PRACTICE_UNAUTHORIZED_401("PRACTICE_UNAUTHORIZED_401", 401, "인증 세션이 만료되었으므로, 재발급해주시기 바랍니다."),
     PRACTICE_FORBIDDEN_403("PRACTICE_FORBIDDEN_403", 403, "해당 즐겨찾기에 대한 접근 권한이 없습니다."),;
 
