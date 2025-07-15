e97a9671bfd76b2be2ee594e0dff92d8b35c50c2
zzuhannn <lunaaeric41@naver.com>
2025-07-15 15:36:58 +0900

    Feat : 아이디 중복 컨트롤러 추가

--- src/main/java/com/example/talky/domain/auth/web/controller/AuthController.java
+++ src/main/java/com/example/talky/domain/auth/web/controller/AuthController.java
@@ -10,10 +10,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -38,4 +35,13 @@ public class AuthController {
                 .status(HttpStatus.OK)
                 .body(SuccessResponse.ok(loginRes));
     }
+
+    @GetMapping("/check")
+    public ResponseEntity<SuccessResponse<?>> checkId(@RequestParam String loginId) {
+        authService.checkIdAvailability(loginId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(SuccessResponse.ok(null));
+    }
+
 }
