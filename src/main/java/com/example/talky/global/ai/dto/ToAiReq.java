1dec1a7dd428e9698eb8889b5a1f7446ad062228
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-07-16 01:20:48 +0900

    Feat: AI 서버에 필요한 DTO 생성

--- src/main/java/com/example/talky/global/ai/dto/ToAiReq.java
+++ src/main/java/com/example/talky/global/ai/dto/ToAiReq.java
@@ -1,10 +1,12 @@
 package com.example.talky.global.ai.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
+@Builder
 public class ToAiReq {
     private List<String> keywords;
     private String context;
