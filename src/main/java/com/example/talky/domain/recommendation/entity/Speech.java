2ff68169b8f153e7933d1d464b69de478c6a11a5
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-07-16 00:40:18 +0900

    Feat: 발화 (시각, 장소) / 발화 (AI 추적용) 엔티티 생성

---
+++ src/main/java/com/example/talky/domain/recommendation/entity/Speech.java
@@ -0,0 +1,23 @@
+package com.example.talky.domain.recommendation.entity;
+
+import com.example.talky.domain.auth.entity.NormalUser;
+import com.example.talky.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Speech extends BaseEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String where;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "normal_id", nullable = false)
+    private NormalUser normalUser;
+}
