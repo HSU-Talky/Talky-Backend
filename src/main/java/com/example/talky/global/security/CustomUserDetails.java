d01448053bb552bb11021a9d322f1855a72e0c29
zzuhannn <lunaaeric41@naver.com>
2025-07-20 13:46:35 +0900

    Feat : 현재 사용자 로직 추가

--- src/main/java/com/example/talky/global/security/CustomUserDetails.java
+++ src/main/java/com/example/talky/global/security/CustomUserDetails.java
@@ -1,6 +1,10 @@
 package com.example.talky.global.security;
 
+import com.example.talky.domain.auth.entity.NormalUser;
 import com.example.talky.domain.auth.entity.User;
+import com.example.talky.domain.auth.exception.UserNotFoundException;
+import com.example.talky.domain.favorites.exception.ForbiddenFavoriteException;
+import com.example.talky.global.response.code.ErrorResponseCode;
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -16,6 +20,13 @@ public class CustomUserDetails implements UserDetails {
 
     public CustomUserDetails(User user) {
         this.user = user;
+    }
+    
+    public NormalUser getNormalUser() {
+        if (!(user instanceof NormalUser)) {
+            throw new UserNotFoundException();
+        }
+        return (NormalUser) user;
     }
 
     @Override
