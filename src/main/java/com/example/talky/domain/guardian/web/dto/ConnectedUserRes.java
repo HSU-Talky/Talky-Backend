7969fecc9637e0957c4f07c52bf8ffdca43ddea7
zzuhannn <lunaaeric41@naver.com>
2025-08-01 21:26:12 +0900

    feat: 연결된 사용자 조회 res

---
+++ src/main/java/com/example/talky/domain/guardian/web/dto/ConnectedUserRes.java
@@ -0,0 +1,24 @@
+package com.example.talky.domain.guardian.web.dto;
+
+import com.example.talky.domain.auth.entity.NormalUser;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ConnectedUserRes {
+    private Long id;
+    private String username;
+    private String loginId;
+    private String connectionCode;
+
+
+    public static ConnectedUserRes from(NormalUser normalUser) {
+        return ConnectedUserRes.builder()
+                .id(normalUser.getId())
+                .username(normalUser.getUsername())
+                .loginId(normalUser.getLoginId())
+                .connectionCode(normalUser.getConnectionCode())
+                .build();
+    }
+}
