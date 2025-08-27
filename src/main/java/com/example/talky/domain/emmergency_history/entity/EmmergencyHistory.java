4866be1ec4914647ecf6370d15afcc82e7552766
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-08-27 16:41:17 +0900

    Feat: 긴급호출 이력 개체 작성

---
+++ src/main/java/com/example/talky/domain/emmergency_history/entity/EmmergencyHistory.java
@@ -0,0 +1,33 @@
+package com.example.talky.domain.emmergency_history.entity;
+
+import com.example.talky.domain.auth.entity.NormalUser;
+import com.example.talky.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EmmergencyHistory extends BaseEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "normal_id")
+    private NormalUser user;
+
+    private String target;
+
+    private String longitude;
+    private String latitude;
+
+    @PrePersist
+    private void prePersist() {
+        this.target = user.getEmergencyTarget();
+    }
+}
