219a700d53316bff2df179fef40ef57efaa9f64f
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-08-29 15:22:41 +0900

    Fix: 긴급호출 조회 API접근 시, 위치정보 접근에 동의한 유저에 한해 도로명주소를 받아오는 필드를 RequestDTO에 추가

--- src/main/java/com/example/talky/domain/user/web/dto/CoordinateReq.java
+++ src/main/java/com/example/talky/domain/user/web/dto/CoordinateReq.java
@@ -11,4 +11,6 @@ public class CoordinateReq {
     private String longitude;
     @NotAccepted
     private String latitude;
+    @NotAccepted(message = "위치정보 접근 동의한 사용자에 한해 긴급호출 사용 시 도로명주소 데이터를 전달하여야 합니다.")
+    private String roadAddress;
 }
