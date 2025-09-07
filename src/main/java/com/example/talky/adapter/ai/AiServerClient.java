5820609afc1c10b42642c60b1fc17196af5a7d18
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-09-07 20:13:12 +0900

    Fix: AiServerClient에서 callAiServer 메서드를 getAiRcmdSentences로 변경, 반환 타입을 ResponseEntity<>에서 AiRcmdRes로 변경

--- src/main/java/com/example/talky/adapter/ai/AiServerClient.java
+++ src/main/java/com/example/talky/adapter/ai/AiServerClient.java
@@ -1,6 +1,7 @@
 package com.example.talky.adapter.ai;
 
 import com.example.talky.adapter.ai.response.RecommendationResponse;
+import com.example.talky.global.ai.dto.AiRcmdRes;
 import com.example.talky.global.ai.dto.ToAiReq;
 import com.example.talky.global.response.SuccessResponse;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface AiServerClient {
     // Object -> SuccessResponse<?>
-    SuccessResponse<RecommendationResponse> callAiServer(ToAiReq req);
+    AiRcmdRes getAiRcmdSentences(ToAiReq req);
 
     // mp3 -> text
     String transcribe(MultipartFile audioFile);
